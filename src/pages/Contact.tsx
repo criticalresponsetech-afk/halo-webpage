9919e2e28c39c202a3f36e3376b4f2ca0decd350
criticalresponsetech-afk <criticalresponsetech@gmail.com>
2025-12-16 15:48:37 -0700

    Converted the React FormEvent imports to type-only to satisfy verbatimModuleSyntax

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react'
+import { type FormEvent, useState } from 'react'
 import { Badge } from '../components/Badge'
 import { Button } from '../components/Button'
 import { Card } from '../components/Card'
