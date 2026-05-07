22ce792f1e1dee5a93b81060faa117e4f66878d2
Critical Response Technologies <criticalresponsetech@gmail.com>
2026-05-07 15:57:08 -0600

    undid email update

--- api/contact.ts
+++ api/contact.ts
@@ -24,7 +24,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const result = await resend.emails.send({
       from: "onboarding@resend.dev",
-      to: "info@haloprotectionsvcs.com", 
+      to: "criticalresponsetech@gmail.com", 
       subject: `Contact - ${name}`,
       html: `
         <p><b>Name:</b> ${name}</p>
